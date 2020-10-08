3e99e33854caa9344f904607bd55d37fc913c900
Dixon <villandixon@yahoo.com>
2020-10-08 23:35:42 +0800

    completed build of hulu clone

---
+++ src/Results.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import "./Results.css";
+import VideoCard from "./VideoCard";
+import axios from "./axios";
+import requests from "./requests";
+import FlipMove from "react-flip-move";
+
+function Results({ selectedOption }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(selectedOption);
+      // console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [selectedOption]);
+
+  return (
+    <div className="results">
+      <FlipMove>
+        {movies.map((movie) => (
+          <VideoCard movie={movie} key={movie.id} />
+        ))}
+      </FlipMove>
+    </div>
+  );
+}
+
+export default Results;
